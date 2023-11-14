638b75d02a522b0843f231e25d3d4ad5b8d00c6c
Johnny Ung <johnny.ung@elev.ga.lbs.se>
2023-11-14 10:57:53 +0100

    goomba whatever

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+   
+
+    public float MovmentSpeedPerSecond = 10.0f;
+    public float MovmentSign = 1.0f;
+
+    public Rigidbody2D myRigidBody = null;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        //copy character velocity from rigidbofy
+        Vector3 characterVelocity = myRigidBody.velocity;
+
+        //sett character velocity to 0
+        characterVelocity.x = 0;
+
+        //add velocity in character right direktion
+        characterVelocity += MovmentSign * MovmentSpeedPerSecond * transform.right.normalized;
+
+        //copy modfied velocity bakc onto rigidbody
+        myRigidBody.velocity = characterVelocity;
     }
 }
